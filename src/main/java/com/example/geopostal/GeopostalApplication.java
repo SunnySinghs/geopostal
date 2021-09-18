923a3d9232a5763d79e5cecb2fcbfca1b6ea3166
sourabhsingh <sourabh.singh@nagarro.com>
2021-09-18 14:01:14 +0530

    Initial commit

---
+++ src/main/java/com/example/geopostal/GeopostalApplication.java
@@ -0,0 +1,13 @@
+package com.example.geopostal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeopostalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeopostalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/geopostal/GeopostalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geopostal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeopostalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
